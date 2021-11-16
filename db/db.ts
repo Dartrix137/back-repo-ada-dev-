be6c108bd8fdd6c78f7ed2ddb5d35067e132b9ed
Dartrix <daniel.ftg@hotmail.com>
2021-11-16 17:32:26 -0500

    c

--- db/db.ts
+++ db/db.ts
@@ -1,6 +1,5 @@
 import { connect } from 'mongoose';
 // const { connect } = require('mongoose');
-
 const conectarBD = async () => {
   return await connect(
     'mongodb+srv://admin:AdminProyectos@gestionproyectosmisiont.1zmu5.mongodb.net/GestionProyectos?retryWrites=true&w=majority'
